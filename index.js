3285b577b7b1306548bef2403a14cc4d4034509f
Andrew Shepherd <andrew.shepherd@dwp.gsi.gov.uk>
2018-09-03 14:19:55 +0100

    feat(nodemon): use nodemon to run while testing
    add shelljs to run basic bash commands and capture the result

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
-
+const shell = require('shelljs')
 
 app.use(express.static('public'))
 //app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/test',  (req, res) =>{
+    console.log('test ---- ')
+    let list = shell.cat('~/pi/index.js');
+    res.send(list)
+  })
+
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
